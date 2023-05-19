21b274eed0a7049336777c512446188d39fbca50
devgustavovasquez <gustavovasquez2002@gmail.com>
2023-05-19 09:32:42 -0300

    [server] feat: enable cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import { TasksController } from "./infra/http/controllers/tasks-controller";
 import { ErrorMiddleware } from "./infra/http/middlewares/error-middleware";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 
 new TasksController(app);
 new ErrorMiddleware(app);
